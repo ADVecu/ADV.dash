90425e95e5c6714a017fa18a0b72ed8628b0e779
Daniel Gaviria <daniel.gaviria.utp@gmail.com>
2023-12-30 22:12:25 -0500

    :sparkles: New fastled library, new welcome animation, trigger welcome animation posible at any time

--- src/controllers/wLed/led_control.cpp
+++ src/controllers/wLed/led_control.cpp
@@ -1,47 +1,72 @@
-#include "Adafruit_NeoPixel.h"
+#include "FastLED.h"
 #include "Arduino.h"
 #include "pcb_definitions.h"
 #include "led_control.h"
 
-Adafruit_NeoPixel strip(LEDS_NUM, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+CRGB strip[LEDS_NUM];
+
+TaskHandle_t ledWelcomeAnimTask;
 
 void led_control::init_leds()
 {
-    strip.begin();
-    strip.show(); // Initialize all pixels to 'off'
-    strip.setBrightness(10);
 
-    xTaskCreatePinnedToCore(this->ledsWelcomeAnimation, "leds_Welcome", 10000, NULL, 1, NULL, APP_CPU_NUM);
+    FastLED.addLeds<WS2812B, LEDS_PIN, GRB>(strip, LEDS_NUM);
+    FastLED.setBrightness(10);
+    FastLED.show();
+
+    xTaskCreatePinnedToCore(this->ledsWelcomeAnimation, "leds_Welcome", 10000, NULL, 1, &ledWelcomeAnimTask, APP_CPU_NUM);
 }
 
 void led_control::ledsWelcomeAnimation(void *pvParameters)
 {
     while (1)
     {
-        int firstPixelHue = 0; // First pixel starts at red (hue 0)
-        for (int a = 0; a < 10; a++)
-        { // Repeat 30 times...
-            for (int b = 0; b < 3; b++)
-            {                  //  'b' counts from 0 to 2...
-                strip.clear(); //   Set all pixels in RAM to 0 (off)
-                // 'c' counts up from 'b' to end of strip in increments of 3...
-                for (int c = b; c < strip.numPixels(); c += 3)
-                {
-                    // hue of pixel 'c' is offset by an amount to make one full
-                    // revolution of the color wheel (range 65536) along the length
-                    // of the strip (strip.numPixels() steps):
-                    int hue = firstPixelHue + c * 65536L / strip.numPixels();
-                    uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-                    strip.setPixelColor(c, color);                       // Set pixel 'c' to value 'color'
-                }
-                strip.show();                // Update strip with new contents
-                delay(50);                 // Pause for a moment
-                firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+        static uint8_t hue = 0;
+
+        // First slide the led in one direction
+        for (int i = 0; i < LEDS_NUM; i++)
+        {
+            // Set the i'th led to red
+            strip[i] = CHSV(hue+=5, 255, 255);
+            
+            // Show the leds
+            FastLED.show();
+            // now that we've shown the leds, reset the i'th led to black
+             //strip[i] = CRGB::Black;
+            for (int i = 0; i < LEDS_NUM; i++)
+            {
+                strip[i].nscale8(250);
             }
+            // Wait a little bit before we loop around and do it again
+            delay(50);
         }
 
-        strip.clear();
-        strip.show();
-        vTaskDelete(NULL);
+        delay(500);
+
+        for (int i = (LEDS_NUM)-1; i >= 0; i--)
+        {
+            // Set the i'th led to red
+            strip[i] = CHSV(hue+=5, 255, 255);
+            // Show the leds
+            FastLED.show();
+            // now that we've shown the leds, reset the i'th led to black
+             strip[i] = CRGB::Black;
+             for (int i = 0; i < LEDS_NUM; i++)
+            {
+                strip[i].nscale8(250);
+            }
+            // Wait a little bit before we loop around and do it again
+            delay(25);
+        }
+
+        
+        FastLED.clear();
+        FastLED.show();
+        vTaskSuspend(NULL);
     }
+}
+
+void led_control::triggerWelcomeAnimation()
+{
+    vTaskResume(ledWelcomeAnimTask);
 }
--- src/controllers/wLed/led_control.h
+++ src/controllers/wLed/led_control.h
@@ -1,11 +1,14 @@
 #ifndef LED_CONTROL_H
 #define LED_CONTROL_H
 
+
 class led_control
 {
+
 public:
     void init_leds();
     static void ledsWelcomeAnimation(void *pvParameters);
+    void triggerWelcomeAnimation();
 };
 
 #endif // LED_CONTROL_H
