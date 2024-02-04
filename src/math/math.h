893acb389637f316b1856b3a6a885ad2c61e13ef
Daniel Gaviria <daniel.gaviria.utp@gmail.com>
2024-02-04 09:49:39 -0500

    :poop: For now only for test

---
+++ src/math/math.h
@@ -0,0 +1,8 @@
+// Description: This file contains the declarations of math functions.
+
+#ifndef MATH_H
+#define MATH_H
+
+float mapD(float x, float in_min, float in_max, float out_min, float out_max);
+
+#endif // MATH_H
